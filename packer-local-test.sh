d7602ad29ad973569f8dec9fe08ef62f50c1dcf3
Alvaro Miranda <kikitux@z2-3.kikitux.net>
2015-10-07 14:21:21 -0700

    updatehelp for -h

--- packer-local-test.sh
+++ packer-local-test.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+print_help(){
+cat <<EOF
+help goes
+here
+mutiline
+EOF
+}
+
+#if no arguments, or -h anywhere, we print help and exit
+if [ $# -eq 0 ] || [[ "$@" =~ "-h" ]];then
+  print_help
+  exit 0
+fi
+
 GHTEST="`curl -sS -I https://api.github.com 2>&1`"
 RET=$?
 
@@ -10,33 +24,33 @@ if [ ${RET} -ne 0 ]; then
   exit ${RET}
 fi
 
-if [ ${1} ];then
-  PR="${1}"
-else
-  unset PR
-fi
-
 if [ -d packer ];then
   pushd packer
-  git fetch
   git clean -fdx
+  git checkout master
+  git pull -f --prune origin HEAD
+  git fetch --all --tags
   popd
 else
   git clone https://github.com/mitchellh/packer.git
   pushd packer
   git config --add remote.origin.fetch '+refs/pull/*/head:refs/remotes/origin/pr/*'
+  git checkout master
+  git pull -f --prune origin HEAD
   git fetch --all --tags
   popd
 fi
 
-if [ ${PR} ];then
-  echo "Will checkout to pr/${PR}"
-  pushd packer
-  git checkout pr/${PR}
-  popd
-else
+if [ "${1}" == "master" ] || [ "${1}" -eq 0 ] ;then
+  CHECKOUT="master"
   echo "Will checkout to master"
   pushd packer
   git checkout master
+  popd
+elif [ "${1}" ];then
+  CHECKOUT="pr/${1}"
+  echo "Will checkout to ${CHECKOUT}"
+  pushd packer
+  git checkout ${CHECKOUT}
   popd
 fi
